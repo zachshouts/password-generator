ece7f0d762537c4d84f2dd4876d74a9403a519fb
zachshouts <117788174+zachshouts@users.noreply.github.com>
2022-12-22 09:58:03 -0600

    Added generateParams function to fill params array with accepted characters

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M
 // numbers 
 var nums = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
 
+var params = [];
+
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -31,7 +33,7 @@ function writePassword() {
 // Your work starts here
 function generatePassword() {
     askQuestions();
-
+    generateParams();
 
     return password;
 }
@@ -48,9 +50,24 @@ function askQuestions() {
     if (!useSpecialChars && !useNums && !useLowerCase && !useUpperCase) {
         alert("Minimum of one category must be selected!");
         askQuestions();
-    } else if (numOfChars < 8 || numOfChars > 128) {
-        alert("Number of characters must be between 8 - 128!");
-        askQuestions();
+    }
+}
+
+function generateParams() {
+    if (useSpecialChars) {
+        params.concat(specialChars);
+    }
+
+    if (useNums) {
+        params.concat(nums);
+    }
+
+    if (useLowerCase) {
+        params.concat(lowerCase);
+    }
+
+    if (useUpperCase) {
+        params.concat(upperCase);
     }
 }
 
